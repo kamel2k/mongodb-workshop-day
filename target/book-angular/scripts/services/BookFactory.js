a82359dbdf4f5571415d7dea687fd699d9cbb894
kamel2k <najj99k@VDFW1RKQB5J.prod.axabanque.int>
2015-01-29 18:22:58 +0100

    suppression des targets

--- target/book-angular/scripts/services/BookFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('bookangular').factory('BookResource', function($resource){
-    var resource = $resource('rest/books/:BookId',{BookId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});

65db16f9ccc941f253e081355ae21d759df4a543
unknown <najj99k@VDFW1RKQB5J.prod.axabanque.int>
2015-01-29 16:21:02 +0100

    suppression de l'application

--- application/book-angular-sql/src/main/java/fr/jasmineconseil/bookangular/rest/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package fr.jasmineconseil.bookangular.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application
-{
-}
--- application/book-angular-sql/src/main/webapp/scripts/services/AuthorFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('bookangular').factory('AuthorResource', function($resource){
-    var resource = $resource('rest/authors/:AuthorId',{AuthorId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
